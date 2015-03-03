ef445292dadd71967e9fd522cd8a10ebda2794ef
Bastiaan Quast <bquast@gmail.com>
2015-03-03 13:52:09 +0100

    setup

---
+++ R/rca.R
@@ -0,0 +1,9 @@
+#' Revealed Comparative Advantage
+#' 
+#' @name rca
+#' @param x A Leontief decomposed Inter-Country Input Output table as created by decompr
+#' @export
+
+rca <- function ( data, country, sector ) {
+  
+}
