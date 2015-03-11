af00274d462dcd0db0e7342ea691870b9e4493d3
Bastiaan Quast <bquast@gmail.com>
2015-03-11 14:37:55 +0100

    fix docu problem

--- R/minus_block_matrix.R
+++ R/minus_block_matrix.R
@@ -1,6 +1,6 @@
 #' Minus Block matrix
 #' 
-#' @name block_matrix
+#' @name minus_block_matrix
 #' @param x a square matrix
 #' @param s the dimensions (width = height) of the block matrix being dropped over the diagonal of matrix x
 #' @export
