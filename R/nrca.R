a48035b3b670b36b63383670f79b39759280d8fb
Bastiaan Quast <bquast@gmail.com>
2015-03-23 14:53:03 +0100

    fix length sector to N and rename data argument to x

--- R/nrca.R
+++ R/nrca.R
@@ -1,73 +1,73 @@
 #' New Revealed Comparative Advantage
 #' 
 #' @name nrca
-#' @param data A decomposed Inter-Country Input Output table as created by decompr
+#' @param x A decomposed Inter-Country Input Output table as created by decompr
 #' @param country target country
 #' @param sector target sector
 #' @export
 
-nrca <- function ( data, country, sector ) {
+nrca <- function ( x, country, sector ) {
   
-  if ( attr(data, "decomposition") == "wwz" ) {
+  if ( attr(x, "decomposition") == "wwz" ) {
     
     # remove exports to self
-    data <- data[ which(!data["Exporting_Country"] == data["Importing_Country"]) , ]
+    x <- x[ which(!x["Exporting_Country"] == x["Importing_Country"]) , ]
     
     # export of sector of country
-    Eij <- sum( data[ which( data["Exporting_Country"] == country & data["Exporting_Industry"] == sector ), ]["DViX_Fsr"] )
+    Eij <- sum( x[ which( x["Exporting_Country"] == country & x["Exporting_Industry"] == sector ), ]["DViX_Fsr"] )
     
     # exports from country of sectors
-    Eit <- sum( data[ which( data["Exporting_Country"] == country ), ]["DViX_Fsr"] )
+    Eit <- sum( x[ which( x["Exporting_Country"] == country ), ]["DViX_Fsr"] )
     
     # exports of sector of countries
-    Enj <- sum( data[ which( data["Exporting_Industry"] == sector ), ]["DViX_Fsr"] )
+    Enj <- sum( x[ which( x["Exporting_Industry"] == sector ), ]["DViX_Fsr"] )
     
     # exports of sectors of country
-    Ent <- sum( data["DViX_Fsr"] )
+    Ent <- sum( x["DViX_Fsr"] )
     
-  } else if ( attr(data, "long") == TRUE ) {
+  } else if ( attr(x, "long") == TRUE ) {
     
     # remove all exports
-    data <- data[ which(data["Source_Country"] == data["Using_Country"]) , ]
+    x <- x[ which(x["Source_Country"] == x["Using_Country"]) , ]
     
     # export of sector of country
-    Eij <- sum( data[ which( data["Source_Country"] == country & data["Source_Industry"] == sector ), ]["FVAX"] )
+    Eij <- sum( x[ which( x["Source_Country"] == country & x["Source_Industry"] == sector ), ]["FVAX"] )
     
     # exports from country of sectors
-    Eit <- sum( data[ which( data["Source_Country"] == country ), ]["FVAX"] )
+    Eit <- sum( x[ which( x["Source_Country"] == country ), ]["FVAX"] )
     
     # exports of sector of countries
-    Enj <- sum( data[ which( data["Source_Industry"] == sector ), ]["FVAX"] )
+    Enj <- sum( x[ which( x["Source_Industry"] == sector ), ]["FVAX"] )
     
     # exports of sectors of country
-    Ent <- sum( data["FVAX"] )
+    Ent <- sum( x["FVAX"] )
     
   } else {
     
     # extract attributes
-    k      <- attr(data, "k")
-    i      <- attr(data, "i")
+    k      <- attr(x, "k")
+    i      <- attr(x, "i")
     G <- length(k)
     N <- length(i)
     
     # remove exports to self
-    data <- minus_block_matrix(data, length(sector) )
+    x <- minus_block_matrix(x, N )
     
     # match country and sector string with positions in list
     country_position <- match(country, k)
     sector_position  <- match(sector,  i)
     
     # exports of sector from country
-    Eij <- rowSums(data[(((country_position-1)*N)+sector_position),])
+    Eij <- rowSums(x[(((country_position-1)*N)+sector_position),])
     
     # exports from country
-    Eit <- sum( rowSums( data[((country_position*N)-N+1):(country_position*N),] ) )
+    Eit <- sum( rowSums( x[((country_position*N)-N+1):(country_position*N),] ) )
     
     # exports of sector from countries
-    Enj <- sum( rowSums( data[((seq(1:N)-1)*G)+sector_position,] ) )
+    Enj <- sum( rowSums( x[((seq(1:N)-1)*G)+sector_position,] ) )
     
     # exports of sectors from countries
-    Ent <- sum( rowSums( data ))
+    Ent <- sum( rowSums( x ))
     
   }
   
