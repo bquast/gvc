d01983d499668f5f74cd5abdaaec9900111d8019
Bastiaan Quast <bquast@gmail.com>
2015-03-26 19:26:58 +0100

    fix repetition error

--- R/minus_block_matrix.R
+++ R/minus_block_matrix.R
@@ -9,7 +9,7 @@ minus_block_matrix <- function( x, s ) {
   
   if (!dim(x)[1] == dim(x)[2]) stop("x is not a square matrix")
   
-  for (j in 1:s) {
+  for (j in 1:(dim(x)[1] %/% s) ) {
     p <- seq( ((j-1)*s + 1), j*s )
     x[p, p] <- 0
   }
