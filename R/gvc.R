eef088cedcb7a4cac1ab43c1da3c41cb650feb33
Bastiaan Quast <bquast@gmail.com>
2015-04-08 09:49:34 +0200

    add package file for documentation etc.

---
+++ R/gvc.R
@@ -0,0 +1,8 @@
+#' @name gvc
+#' @docType package
+#' @title Global Value Chain analysis
+#' @author
+#' Bastiaan Quast \email{bquast@@gmail.com}
+#' Victor Kummritz
+#' @seealso http://qua.st/decompr
+#' @references {Wang, Zhi, Shang-Jin Wei, and Kunfu Zhu. Quantifying international production sharing at the bilateral and sector levels. No. w19677. National Bureau of Economic Research, 2013.}
