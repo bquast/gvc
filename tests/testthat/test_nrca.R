26ee6ad5aa0764703b88bba5f248ea0eb9d1a296
Bastiaan Quast <bquast@gmail.com>
2015-04-08 14:05:49 +0200

    small fixes

--- tests/testthat/test_nrca.R
+++ tests/testthat/test_nrca.R
@@ -1,26 +1,24 @@
 # test_nrca.R
 
-# load the decompr package
+# load the decompr packages
 library(decompr)
+library(gvc)
 
 # load the example data set
 data(leather)
 
-# create a leontief decomposed data set
+# define context
+context("output format")
+
+# create a leontief long decomposed data set
 l <- decomp(inter,
             final,
             countries,
             industries,
             out)
 
-# load the package
-library(gvc)
-
-# apply the Exporting to Re-export
+# apply the nrca to leontief
 lnrca <- nrca( l )
-
-# define context
-context("output format")
 
 test_that("output size matches", {
   expect_equal( length(lnrca), 9 )
@@ -30,3 +28,5 @@ test_that("output values match", {
   expect_equal( lnrca[1], 1.2676927, tolerance = .002)
   expect_equal( lnrca[9], 1.9843574, tolerance = .002)
 })
+
+# define short context
