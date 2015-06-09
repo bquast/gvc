6e01efaa28402342414087680d8a56a815b3e751
Bastiaan Quast <bquast@gmail.com>
2015-06-09 14:35:20 +0200

    Removed last hickups

--- R/ffddva.R
+++ R/ffddva.R
@@ -1,17 +1,17 @@
 #' Foreign Final Demand Domestic Value Added
-#' 
+#'
 #' @name ffddva
 #' @param x A Leontief decomposed Inter-Country Input Output table as created by decompr, which should be post multiplied with final demand (using the parameter: post="final_demand")
 #' @param aggregate should dfddva be aggregated along source industries to a national sum?
 #' @export
 #' @import decompr
-#' @examples 
+#' @examples
 #' # load the decompr package
 #' library(decompr)
-#' 
+#'
 #' # load example data
 #' data(leather)
-#' 
+#'
 #' # create a leontief decomposed data set
 #' l <- decomp(inter,
 #'             final,
@@ -20,42 +20,43 @@
 #'             out,
 #'             method = "leontief",
 #'             post = "final_demand")
-#'  
+#'
 #'  # apply ffddva
 #'  ffddva( l )
 
 ffddva <- function ( x, aggregate=FALSE ) {
-  
+
   # read attributes
   k      <- attr(x, "k")
   i      <- attr(x, "i")
   # rownam <- attr(x, "rownam")
-  G <- length(k)
-  N <- length(i)
-  
+  G  <- length(k)
+  N  <- length(i)
+  GN <- G*N
+
   # transform back to 2dim x 2dim matrix
-  x <- matrix(x[,4], nrow=G*N, byrow=TRUE)
-  
+  x <- matrix(x[,4], nrow=GN, byrow=TRUE)
+
   # remove exports to self
   x <- diagonals::minus_rectangle_matrix( x, steps=G )
-  
+
   # sum across rows
   x <- rowSums(x)
-  
+
   # create output format
   x <- data.frame(Source_Country = rep(k, each=N), Source_Industry = rep(i, times=G), ffddva = x)
-  
+
   # aggregate or not
   if (aggregate) {
     f <- as.factor(x[,1])
-    
+
     x <- tapply(x[,3], f, sum)
-    
-    x <- data.frame(Source_Country = k, ffddva = x)
-    
+
+    x <- data.frame(Source_Country = row.names(x), ffddva = x)
+
     row.names(x) <- NULL
   }
 
   return(x)
-  
+
 }
