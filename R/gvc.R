dd06e871b7ad92b6ed81785cbc567e2ffa1fb9e0
Bastiaan Quast <bquast@gmail.com>
2015-10-06 14:05:02 +0200

    add citation request

--- R/gvc.R
+++ R/gvc.R
@@ -7,3 +7,8 @@
 #' @seealso http://qua.st/decompr
 #' @references {Wang, Zhi, Shang-Jin Wei, and Kunfu Zhu. Quantifying international production sharing at the bilateral and sector levels. No. w19677. National Bureau of Economic Research, 2013.}
 NULL
+.onAttach <- function(...) {
+  packageStartupMessage('If you use gvc for data analysis,
+please cite both R and decompr,
+using citation() and citation("gvc") respectively.
+                        ')}
