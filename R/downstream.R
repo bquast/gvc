0f52109a5ca36b23ec8be43c170b8e173e00192b
Bastiaan Quast <bquast@gmail.com>
2015-10-30 08:53:50 +0100

    fix input description

--- R/downstream.R
+++ R/downstream.R
@@ -1,7 +1,7 @@
 #' Downstreamness
 #' 
 #' @name downstream
-#' @param x A Leontief decomposed Inter-Country Input Output table as created by decompr, which should be post multiplied with final demand (using the parameter: post="final_demand")
+#' @param x an object of class "decompr" as created using the load_tables_vectors() function from the decompr package.
 #' @export
 #' @import decompr
 #' @examples 
@@ -13,10 +13,10 @@
 #' 
 #' # create a leontief decomposed data set
 #' l <- load_tables_vectors(inter,
-#'             final,
-#'             countries,
-#'             industries,
-#'             out)
+#'                          final,
+#'                          countries,
+#'                          industries,
+#'                          out        )
 #'  
 #'  # apply downstream
 #'  downstream( l )
