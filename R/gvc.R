124557a46780fc6041528aed872e4f51a47ce2f5
Bastiaan Quast <bquast@gmail.com>
2020-01-07 11:36:58 +0100

    update citation message

--- R/gvc.R
+++ R/gvc.R
@@ -9,7 +9,5 @@
 #' @references {Wang, Zhi, Shang-Jin Wei, and Kunfu Zhu. Quantifying international production sharing at the bilateral and sector levels. No. w19677. National Bureau of Economic Research, 2013.}
 NULL
 .onAttach <- function(...) {
-  packageStartupMessage('If you use gvc for data analysis,
-please cite both R and gvc,
-using citation() and citation("gvc") respectively.
-                        ')}
+  packageStartupMessage('Please consider citing both R and gvc,
+using citation() and citation("gvc")                        ')}
